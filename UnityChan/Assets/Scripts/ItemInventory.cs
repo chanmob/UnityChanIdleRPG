f0e380d5839857a89ec19649ea71b5ba0131c6d4
Chanmob <chanmob@gambridzy.com>
2020-12-27 17:04:12 +0900

    아이템인벤, 스킬인벤 추가

---
+++ UnityChan/Assets/Scripts/ItemInventory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInventory : MonoBehaviour
+{
+    private List<Item> _list_Items;
+
+    private void Awake()
+    {
+        _list_Items = new List<Item>();
+    }
+
+    public void AddItem(Item item)
+    {
+        _list_Items.Add(item);
+    }
+
+    public void RefreshInventory()
+    {
+        int len = _list_Items.Count;
+
+        for (int i = 0; i < len; i++)
+        {
+            
+        }
+    }
+}
---
+++ UnityChan/Assets/Scripts/SkillInventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillInventory : MonoBehaviour
+{
+    private List<Skill> _list_Skills;
+
+    private void Awake()
+    {
+        _list_Skills = new List<Skill>();
+    }
+
+    public void AddItem(Skill skill)
+    {
+        _list_Skills.Add(skill);
+    }
+
+    public void RefreshInventory()
+    {
+        int len = _list_Skills.Count;
+
+        for (int i = 0; i < len; i++)
+        {
+            
+        }
+    }
+}
